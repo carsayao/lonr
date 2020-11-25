1971d70625daefbe8bbfa46ffa15695513e9b90d
Carlos Miguel Sayao <carsayao@pdx.edu>
2020-11-24 23:53:08 -0800

    built list of transcripts
    need to do df and pickle

--- static/texts/scraps.py
+++ static/texts/scraps.py
@@ -6,6 +6,7 @@ from bs4 import BeautifulSoup
 flinks = "scraps-links.txt"
 
 def get_urls():
+    """ Get urls, check against what's already been scraped and blocked """
     url = "https://scrapsfromtheloft.com/stand-up-comedy-scripts/"
     r = requests.get(url)
     soup = BeautifulSoup(r.text, 'html.parser')
@@ -20,6 +21,8 @@ def get_urls():
         done = f.readlines()
     # Only add to file what's not already in there
     new_urls = [f for f in fetched_urls if not any(f in d for d in done)]
+    print(f"Not in list:")
+    [print(n) for n in new_urls]
     # for f in fetched_urls:
     #     if not any(f in d for d in done):
     #         new_urls.append(f)
@@ -27,14 +30,17 @@ def get_urls():
     with open("scraps-block-links.txt") as f:
         block_urls = f.readlines()
     new_urls = [n for n in new_urls if not any(n in b for b in block_urls)]
+    print(f"{len(new_urls)} new links to download")
+    [print(n) for n in new_urls]
 
     # Write these new links to file
-    print(f"{len(new_urls)} new links")
     with open(flinks, 'a') as f:
-        f.writelines('\n'.join(new_urls))
-        f.write('\n')
+        f.writelines('\n'.join(new_urls)) if os.stat(flinks).st_size == 0 else f.writelines('\n'.join(new_urls)+'\n')
+
+    return new_urls
 
 def get_transcript(url):
+    """ Get transcript from url. Parse out name, act, date uploaded. """
     path = urlparse(url)
     url_path = re.search(r"[a-z].*[full-transcript]", str(path.path)).string.split('/')[1:-1]
     date = '-'.join(url_path[:3])
@@ -45,13 +51,17 @@ def get_transcript(url):
     r = requests.get(url)
     soup = BeautifulSoup(r.text, 'html.parser')
     raw_html = soup.find('div', attrs = {'class':'elementor-widget-theme-post-content'})
-    text = ""
-    for p in raw_html.find_all('p'):
-        text += f"{p.get_text()}\n"
-    
-    write_transcript(name, date, title, fname, text)
 
-def write_transcript(name, date, title, fname, text):
+    # TODO: this is some test text
+    text = "content"
+    # text = ""
+    # for p in raw_html.find_all('p'):
+        # text += f"{p.get_text()}\n"
+    
+    return [name, date, title, fname, text]
+
+def write_transcript(name, fname, text):
+    """ """
     if not os.path.exists(name):
         os.makedirs(name)
     if os.path.isfile(fname):
@@ -61,11 +71,18 @@ def write_transcript(name, date, title, fname, text):
             f.write(text)
         print(f"[✓] {fname} written")
 
+def save(transcripts):
+    """ Pickle the data
+    """
+    [write_transcript(t[0], t[3], t[4]) for t in transcripts]
 
-get_urls()
-
-with open(flinks) as f:
-    urls = f.readlines()
+urls = get_urls()
+# with open(flinks) as f:
+    # urls = f.readlines()
 urls = [i.strip() for i in urls]
-for u in urls:
-    get_transcript(u)
+# Turn into list comprehension of all transcripts
+transcripts = [get_transcript(u) for u in urls]
+save(transcripts)
+# print(transcripts)
+# for u in urls:
+#     get_transcript(u)
